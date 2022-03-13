b9a621ce923530e8d25c32c09b93469b63143334
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-03-13 17:44:44 -0400

    fix uploads file

--- utils/multer.js
+++ utils/multer.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, "../../app/uploads");
+            cb(null, "../../uploads");
         },
         filename: function (req, file, cb) {
             cb(null, file.originalname);
